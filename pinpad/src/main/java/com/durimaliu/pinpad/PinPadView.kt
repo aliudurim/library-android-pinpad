fbf3e41aa4c53f82ff4c0320f8754cbad0bc12c2
Durim Aliu <durimaliu@zombiesoup.com>
2018-12-08 17:22:36 +0100

    Set programmatically adding constraints in Constraint Layout.

--- pinpad/src/main/java/com/durimaliu/pinpad/PinPadView.kt
+++ pinpad/src/main/java/com/durimaliu/pinpad/PinPadView.kt
@@ -2,8 +2,10 @@ package com.durimaliu.pinpad
 
 import android.content.Context
 import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 
 class PinPadView @JvmOverloads
 constructor(
@@ -13,9 +15,22 @@ constructor(
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
     init {
-        LayoutInflater.from(context).inflate(
+        val view = LayoutInflater.from(context).inflate(
             R.layout.view_pin_pad,
             this, false
         )
+
+        val set = ConstraintSet()
+        addView(view)
+
+        set.clone(this)
+        set.match(view, this)
     }
+}
+
+fun ConstraintSet.match(view: View, parentView: View) {
+    this.connect(view.id, ConstraintSet.TOP, parentView.id, ConstraintSet.TOP)
+    this.connect(view.id, ConstraintSet.START, parentView.id, ConstraintSet.START)
+    this.connect(view.id, ConstraintSet.END, parentView.id, ConstraintSet.END)
+    this.connect(view.id, ConstraintSet.BOTTOM, parentView.id, ConstraintSet.BOTTOM)
 }
